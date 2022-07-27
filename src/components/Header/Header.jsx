e54062bec46deaeceec079621ff9fe57f37991be
Anushka7310 <anushkaguptably@gmail.com>
2022-07-27 23:41:48 +0530

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import "./Header.css";
+
+function Header() {
+    return (
+    <header>
+
+        <h1>
+        <MenuIcon />
+        Bird App</h1>
+    </header>
+    );
+}
+
+
+export default Header;
